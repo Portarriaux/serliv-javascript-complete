1aff7eeb6ac5408eed0cf968ecd9c47ebd07bdd7
Portarriaux <jhonatancaetano14@hotmail.com>
2025-07-27 15:15:02 -0300

    Record removal of files after renaming/reorganizing the structure

--- Seção-02-RLP/falsy-truh.js
+++
@@ -1,8 +0,0 @@
-// Valores considerados falsos em Js: Falsy -0 "", NaN, undefined, null, false.
-// truth - Todos os decimais. 
-
-if("Ok") {
-   console.log("true")
-} else {
-   console.log("false")
-}
--- Seção-02-RLP/variaveis.js
+++
@@ -1,4 +0,0 @@
-// var, let, const 
-// var -> ES5 - cross-browser
-// let e const - ES2015
-
